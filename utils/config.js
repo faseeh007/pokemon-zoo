cc332598c5f8f6ead0425800ac3cc5693eda0450
faseeh007 <faseehahmedrocks26@gmail.com>
2022-08-08 14:18:07 +0530

    commit

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+// require("dotenv").config()
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+module.exports = {
+  MONGODB_URI,
+  PORT,
+  JWT_SECRET,
+};
