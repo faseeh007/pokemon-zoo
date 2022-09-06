733edefef85d51d95877415eb5dc54c7cd5b436f
faseehahmed <54709992+faseehahmed26@users.noreply.github.com>
2022-09-06 12:13:19 +0530

    Updates

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,17 +12,17 @@ const Footer = () => {
   return (
     <div>
       <footer tw="flex flex-row items-center justify-center w-full text-gray-900 bg-blue-100 text-sm mt-4">
-        Developed by Akshaj Verma.
-        <a href="https://www.linkedin.com/in/akshajverma7/">
+        Developed by Faseeh Ahmed.
+        <a href="">
           <LinkedinIcon tw="h-3 w-3 mx-1" />
         </a>
-        <a href="https://medium.com/@theairbend3r">
+        <a href="">
           <MediumIcon tw="h-3 w-3 mx-1" />
         </a>
-        <a href="https://github.com/theairbend3r/">
+        <a href="">
           <GithubIcon tw="h-3 w-3 mx-1" />
         </a>
-        <a href="https://twitter.com/theairbend3r">
+        <a href="">
           <TwitterIcon tw="h-3 w-3 mx-1" />
         </a>
       </footer>
